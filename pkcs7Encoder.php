a5ebae6c182a171f008c51f19ec689fedef59070
abelzhu <shengbenzhu@gmail.com>
2018-03-14 10:24:06 +0800

    修复加解密方法不对的bug

---
+++ pkcs7Encoder.php
@@ -0,0 +1,96 @@
+<?php
+
+include_once "errorCode.php";
+
+/**
+ * Prpcrypt class
+ *
+ * 提供接收和推送给公众平台消息的加解密接口.
+ */
+class Prpcrypt
+{
+	public $key;
+
+	function __construct($k)
+	{
+		$this->key = base64_decode($k . "=");
+	}
+
+	/**
+	 * 对明文进行加密
+	 * @param string $text 需要加密的明文
+	 * @return string 加密后的密文
+	 */
+	public function encrypt($text, $corpid)
+	{
+
+        try {
+            //获得16位随机字符串，填充到明文之前
+            $random = $this->getRandomStr();
+            $text = $random.pack('N', strlen($text)).$text.$corpid;
+            // 网络字节序
+            $iv = substr($this->key, 0, 16);
+            $encrypted = openssl_encrypt($text, 'aes-256-cbc', $this->key, $options= 1 | OPENSSL_NO_PADDING, $iv);
+            return array(0, base64_encode($encrypted));
+		} catch (Exception $e) {
+			print $e;
+			return array(ErrorCode::$EncryptAESError, null);
+		}
+	}
+
+	/**
+	 * 对密文进行解密
+	 * @param string $encrypted 需要解密的密文
+	 * @return string 解密得到的明文
+	 */
+	public function decrypt($encrypted, $corpid)
+	{
+
+		try { 
+            //使用BASE64对需要解密的字符串进行解码
+            $ciphertext_dec = base64_decode($encrypted);
+            $iv = substr($this->key, 0, 16);
+            $decrypted = openssl_decrypt($ciphertext_dec, 'aes-256-cbc', $this->key, $options = 1 | OPENSSL_NO_PADDING, $iv);
+		} catch (Exception $e) {
+			return array(ErrorCode::$DecryptAESError, null);
+		}
+
+
+		try {
+			//去除16位随机字符串,网络字节序和AppId
+			if (strlen($decrypted) < 16) return "";
+			$content = substr($decrypted, 16, strlen($decrypted));
+			$len_list = unpack("N", substr($content, 0, 4));
+			$xml_len = $len_list[1];
+			$xml_content = substr($content, 4, $xml_len);
+			$from_corpid = substr($content, $xml_len + 4);
+		} catch (Exception $e) {
+			print $e;
+			return array(ErrorCode::$IllegalBuffer, null);
+		}
+		if ($from_corpid != $corpid)
+			return array(ErrorCode::$ValidateCorpidError, null);
+		return array(0, $xml_content);
+
+	}
+
+
+	/**
+	 * 随机生成16位字符串
+	 * @return string 生成的字符串
+	 */
+	function getRandomStr()
+	{
+
+		$str = "";
+		$str_pol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+		$max = strlen($str_pol) - 1;
+		for ($i = 0; $i < 16; $i++) {
+			$str .= $str_pol[mt_rand(0, $max)];
+		}
+		return $str;
+	}
+
+}
+
+?>
