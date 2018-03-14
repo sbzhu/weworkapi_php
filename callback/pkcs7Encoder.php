364d83c6eba9f335890123c23f6ccd3986cdcfdd
abelzhu <shengbenzhu@gmail.com>
2018-03-14 10:25:46 +0800

    修复加解密方法不对的bug

--- callback/pkcs7Encoder.php
+++ callback/pkcs7Encoder.php
@@ -3,55 +3,6 @@
 include_once "errorCode.php";
 
 /**
- * PKCS7Encoder class
- *
- * 提供基于PKCS7算法的加解密接口.
- */
-class PKCS7Encoder
-{
-	public static $block_size = 32;
-
-	/**
-	 * 对需要加密的明文进行填充补位
-	 * @param $text 需要进行填充补位操作的明文
-	 * @return 补齐明文字符串
-	 */
-	function encode($text)
-	{
-		$block_size = PKCS7Encoder::$block_size;
-		$text_length = strlen($text);
-		//计算需要填充的位数
-		$amount_to_pad = PKCS7Encoder::$block_size - ($text_length % PKCS7Encoder::$block_size);
-		if ($amount_to_pad == 0) {
-			$amount_to_pad = PKCS7Encoder::block_size;
-		}
-		//获得补位所用的字符
-		$pad_chr = chr($amount_to_pad);
-		$tmp = "";
-		for ($index = 0; $index < $amount_to_pad; $index++) {
-			$tmp .= $pad_chr;
-		}
-		return $text . $tmp;
-	}
-
-	/**
-	 * 对解密后的明文进行补位删除
-	 * @param decrypted 解密后的明文
-	 * @return 删除填充补位后的明文
-	 */
-	function decode($text)
-	{
-
-		$pad = ord(substr($text, -1));
-		if ($pad < 1 || $pad > PKCS7Encoder::$block_size) {
-			$pad = 0;
-		}
-		return substr($text, 0, (strlen($text) - $pad));
-	}
-
-}
-
-/**
  * Prpcrypt class
  *
  * 提供接收和推送给公众平台消息的加解密接口.
@@ -79,9 +30,6 @@ class Prpcrypt
             $text = $random.pack('N', strlen($text)).$text.$corpid;
             // 网络字节序
             $iv = substr($this->key, 0, 16);
-            //使用自定义的填充方式对明文进行补位填充
-            $pkc_encoder = new Pkcs7Encoder();
-            $text = $pkc_encoder->encode($text);
             $encrypted = openssl_encrypt($text, 'aes-256-cbc', $this->key, $options= 1 | OPENSSL_NO_PADDING, $iv);
             return array(0, base64_encode($encrypted));
 		} catch (Exception $e) {
@@ -109,13 +57,9 @@ class Prpcrypt
 
 
 		try {
-			//去除补位字符
-			$pkc_encoder = new PKCS7Encoder;
-			$result = $pkc_encoder->decode($decrypted);
 			//去除16位随机字符串,网络字节序和AppId
-			if (strlen($result) < 16)
-				return "";
-			$content = substr($result, 16, strlen($result));
+			if (strlen($decrypted) < 16) return "";
+			$content = substr($decrypted, 16, strlen($decrypted));
 			$len_list = unpack("N", substr($content, 0, 4));
 			$xml_len = $len_list[1];
 			$xml_content = substr($content, 4, $xml_len);
