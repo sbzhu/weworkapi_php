4ef1ba1197ab3884cfdfb870cc531e5be838bf41
abelzhu <shengbenzhu@gmail.com>
2018-04-24 15:13:34 +0800

    add paramter $to_invite for batchjobargs

--- api/datastructure/Batch.class.php
+++ api/datastructure/Batch.class.php
@@ -9,6 +9,7 @@ class CallBack {
 
 class BatchJobArgs { 
     public $media_id = null; // string 
+    public $to_invite = null; // bool, 是否邀请新建的成员使用企业微信（将通过微信服务通知或短信或邮件下发邀请，每天自动下发一次，最多持续3个工作日）
     public $callback = null; // CallBack 
 } // class BatchJobRequest
 
