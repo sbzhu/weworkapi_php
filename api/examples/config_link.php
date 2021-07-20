553556b7da126648def513abfe44f35e26863ee8
logmecn <logme@foxmail.com>
2021-07-20 13:50:49 +0800

    Update config_link.php

--- api/examples/config_link.php
+++ api/examples/config_link.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'corpid' => 'wx17a22c6916657c82e',
-    'sec' => '1GjahdG3_Z31gYhaxqbrL90WpwW-IXaS7CPlto3UY4Y4', // AGENT_SECRET
+    'sec' => '1Gjahd***3UY4Y4', // AGENT_SECRET
 ];
